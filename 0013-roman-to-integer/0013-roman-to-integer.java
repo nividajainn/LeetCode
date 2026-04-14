5a958208224566c050ca81d97a2abb601bfb2f90
Nivida Jain <151547641+nividajainn@users.noreply.github.com>
2026-04-14 22:57:55 +0530

    Time: 5 ms (35.37%), Space: 44.8 MB (99.98%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        char chars[] = s.toCharArray(); //given string to array 
+         int result = 0;
+         int i, j;
+         //i j pointer 0 or 1 me 
+         for(i=0, j=1; j<chars.length; i++, j++){
+            //agar i bada hoga j se to add karenge
+            if(map.get(chars[i])>= map.get(chars[j])){
+                result = result + map.get(chars[i]);
+            }
+            //nahi to sub
+            else{
+                 result = result - map.get(chars[i]);
+            }
+         }
+         //or baki ko add kar do
+         result += map.get(chars[i]);
+         return result;
+    }
+}
