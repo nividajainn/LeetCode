e516cad1cd1a54cb067bdef3e432ca90e9991be7
Nivida Jain <151547641+nividajainn@users.noreply.github.com>
2026-06-02 23:39:58 +0530

    Time: 1 ms (99.95%), Space: 77.3 MB (47.37%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,16 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        int maxSum = nums[0]; //first wale ko maxSum maan lo
+        int currSum = 0;  //ek sum count karne ke liye
         
-        int maxSum = nums[0];
-        int currSum = 0;
-
+        //
         for(int i=0; i<nums.length; i++){
-            currSum += nums[i];
-            maxSum = Math.max(maxSum, currSum);
+            currSum += nums[i]; //currsum me number add krte rho 
 
-            if(currSum < 0) currSum = 0;
+            maxSum = Math.max(maxSum, currSum); //or dekhte rho ko maxsum kaha aa rha
+
+            if(currSum < 0) currSum = 0; //agar -ve me aaya to 0 kar do 
         }
-        
 
         return maxSum;
     }
