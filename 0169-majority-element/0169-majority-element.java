a80a8a36ef5dd71f2ac7187e51467b441c53d59e
Nivida Jain <151547641+nividajainn@users.noreply.github.com>
2026-06-05 19:53:44 +0530

    Time: 2 ms (70.19%), Space: 55.4 MB (69.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
-          Arrays.sort(nums);
-        return nums[nums.length / 2]; 
+      int candidate = 0, count = 0;
+
+      for(int num : nums){
+       
+        // Agar count 0 ho gaya
+        // to naya candidate choose karo
+            if (count == 0) {
+            candidate = num;
+            }
+
+            // Same candidate mila to vote badhao
+            if (num == candidate) {
+                count++;
+            }
+            
+            // Different mila to vote ghatao
+            else {
+                count--;
+            }
+       }
+       return candidate;
     }
 }
+
+ /*
+    Majority element appears more than n/2 times.
+    
+    After sorting, Arrays.sort(nums);
+    majority element will always be present return nums[n/2];
+    at middle index.
+ */
