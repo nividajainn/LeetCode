0bbde1b9d77637d88b6d01dbb83c08e027bc1a60
Nivida Jain <151547641+nividajainn@users.noreply.github.com>
2026-06-07 12:30:10 +0530

    Time: 2 ms (99.39%), Space: 47.5 MB (10.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            int complement = target-nums[i];
 
-            //agar num hoga map me to hum wo return kara denge 
-            if (map.containsKey(complement)){
-                return new int[]{map.get(complement), i};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target -  nums[i];
+            // If complement found, return indices
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
             }
-            //nahi hoga to daal denge aage check ke liye
-            else {
-                map.put(nums[i], i);
-            }
+            // Store current element and index
+            map.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No match");
-     }
+        // No pair found
+        return new int[] { -1, -1 };
+    }
 }
